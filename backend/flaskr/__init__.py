4b96080e04245c0f73e707187ca67c4814005f7b
adit05 <66363053+adit05@users.noreply.github.com>
2020-07-07 11:54:23 -0700

    added changes

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -12,21 +12,35 @@ def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
   setup_db(app)
-  
+   
   '''
   @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
   '''
-
+  CORS(app, resources={r"*": {"origins":"*"}})
   '''
   @TODO: Use the after_request decorator to set Access-Control-Allow
   '''
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers','Content-type,Authorization,true')
+    response.headers.add('Access-Control-Allow-Methods','GET,PUT,PATCH,DELETE,POST,OPTIONS')
+    return response
 
   '''
   @TODO: 
   Create an endpoint to handle GET requests 
   for all available categories.
   '''
+  @app.route("/categories", methods=['GET'])
+  def retrive_categories():
+    categories = list(map(Category.format, Category.query.all()))
 
+    if len(categories) == 0:
+        abort(404)
+    return jsonify({
+      'success': True,
+      'categories' : categories
+    )}
 
   '''
   @TODO: 
@@ -40,6 +54,22 @@ def create_app(test_config=None):
   ten questions per page and pagination at the bottom of the screen for three pages.
   Clicking on the page numbers should update the questions. 
   '''
+  @app.route('/questions', methods=['GET'])
+  def retrieve_questions():
+      selection = Question.query.order_by(Question.id).all()
+      current_questions = paginate_questions(request, selection)
+      categories = list(map(Category.format, Category.query.all()))
+      
+      if len(current_questions) == 0:
+            abort(404)
+ 
+      return jsonify({
+          'success': True,
+          'questions': current_questions,
+          'total_questions': len(Question.query.all()),
+          'categories': categories,
+          'current_category': None
+      })
 
   '''
   @TODO: 
