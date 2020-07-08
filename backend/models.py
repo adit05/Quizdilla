c38c04eb555ae5729cbf114e64074660b28fac65
adit05 <66363053+adit05@users.noreply.github.com>
2020-07-08 13:33:08 -0700

    added changes

--- backend/models.py
+++ backend/models.py
@@ -4,9 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 
 database_name = "trivia"
-database_path = "postgres://{}/{}".format('localhost:5432', database_name)
-
-db = SQLAlchemy()
+database_path = 'postgres://{}:{}@{}/{}'.format('postgres', '2725', 'localhost:5432', 'trivia')
+db = SQLAlchemy()  
 
 '''
 setup_db(app)
